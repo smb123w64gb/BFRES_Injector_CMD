7aa0bb5db6c21d5e27002c5e98cf5bf2993212d9
smb123w64gb <smb123w64gb@gmail.com>
2017-07-02 13:56:56 -0700

    Redoing Vertex Atributes

--- BFRES_Injector_CMD/Program.cs
+++ BFRES_Injector_CMD/Program.cs
@@ -16,7 +16,7 @@ namespace BFRES_Injector_CMD
             Console.WriteLine($"\n> BFRES Injector v2.0a\n" +
                                "> Made by SMB123W64GB\n" +
                                 "> Using Syroot.NintenTools.Bfres API\n" +
-                                "> *.bfres *.obj\n");
+                                "> *.bfres *.obj/*.smd\n");
 
             ResFile TargetBFRES = new ResFile(args[0]);
             MeshObj test = new MeshObj();
@@ -39,7 +39,7 @@ namespace BFRES_Injector_CMD
                 //Deal With Mesh
                 uint[] theFaces = meshes[0].faces.ToArray();
                 input.Shapes[0].Meshes[0].SetIndices(theFaces);
-                for (int i = 0; i < input.Shapes[0].Meshes.Count; i++) input.Shapes[0].Meshes.RemoveAt(1);
+                if(input.Shapes[0].Meshes.Count!=1) for (int i = 0; i < input.Shapes[0].Meshes.Count; i++) input.Shapes[0].Meshes.RemoveAt(1);
                 input.Shapes[0].Radius = 10000000000f;
 
                 input.Shapes[0].Meshes[0].SubMeshes.Clear();
@@ -55,26 +55,67 @@ namespace BFRES_Injector_CMD
                 //input.Shapes[0].Name = meshes[0].Name;
                 //Deal with Vertexes
                 VertexBufferHelper helper = new VertexBufferHelper(input.VertexBuffers[0], BO);
-                List<int> tests = new List<int>();
-                for(int i = 0; i < helper.Attributes.Count; i++)
-                {
-                    if (helper.Attributes[i].Name == "_t0" | helper.Attributes[i].Name == "_u1" | helper.Attributes[i].Name == "_u2")
-                        tests.Add(i);
-                }
-                tests.Reverse();
-                foreach (int i in tests) helper.Attributes.RemoveAt(i);
-
-                VertexBufferHelperAttrib vertPos = helper["_p0"];
+                List<VertexBufferHelperAttrib> atrib = new List<VertexBufferHelperAttrib>();
+                VertexBufferHelperAttrib vertPos = new VertexBufferHelperAttrib();
+                vertPos.Name = "_p0";
                 vertPos.Data = meshes[0].verts.ToArray();
                 vertPos.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_16_16_16_16_Single;
-                VertexBufferHelperAttrib vertUV = helper["_u0"];
+                VertexBufferHelperAttrib vertUV = new VertexBufferHelperAttrib();
+                vertUV.Name = "_u0";
                 vertUV.Data = meshes[0].uvs.ToArray();
                 vertUV.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_16_16_Single;
-                VertexBufferHelperAttrib vertNorm = helper["_n0"];
+                VertexBufferHelperAttrib vertNorm = new VertexBufferHelperAttrib();
+                vertNorm.Name = "_n0";
                 vertNorm.Data = meshes[0].norms.ToArray();
+                vertNorm.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_10_10_10_2_SNorm;
+                atrib.Add(vertPos);
+                atrib.Add(vertNorm);
+                atrib.Add(vertUV);
+                helper.Attributes = atrib;
                 input.VertexBuffers[0] = helper.ToVertexBuffer();
 
                 }
+            public void InjectMeshRigged(Model input, Syroot.BinaryData.ByteOrder BO)
+            {
+                //Deal With Mesh
+                uint[] theFaces = meshes[0].faces.ToArray();
+                input.Shapes[0].Meshes[0].SetIndices(theFaces);
+                if (input.Shapes[0].Meshes.Count != 1) for (int i = 0; i < input.Shapes[0].Meshes.Count; i++) input.Shapes[0].Meshes.RemoveAt(1);
+                input.Shapes[0].Radius = 10000000000f;
+
+                input.Shapes[0].Meshes[0].SubMeshes.Clear();
+                Bounding LeeT = new Bounding();
+                LeeT.Center = new Syroot.Maths.Vector3F(0, 0, 0);
+                LeeT.Extent = new Syroot.Maths.Vector3F(10000000000f, 10000000000f, 10000000000f);
+                for (int i = 0; i < input.Shapes[0].SubMeshBoundings.Count; i++) input.Shapes[0].SubMeshBoundings[i] = LeeT;
+
+                SubMesh Setup = new SubMesh();
+                Setup.Count = (uint)meshes[0].faces.Count;
+                Setup.Offset = 0;
+                for (int vvv = 0; vvv < 4; vvv++) input.Shapes[0].Meshes[0].SubMeshes.Add(Setup);
+                //input.Shapes[0].Name = meshes[0].Name;
+                //Deal with Vertexes
+                VertexBufferHelper helper = new VertexBufferHelper(input.VertexBuffers[0], BO);
+                List<VertexBufferHelperAttrib> atrib = new List<VertexBufferHelperAttrib>();
+                VertexBufferHelperAttrib vertPos = new VertexBufferHelperAttrib();
+                vertPos.Name = "_p0";
+                vertPos.Data = meshes[0].verts.ToArray();
+                vertPos.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_16_16_16_16_Single;
+                VertexBufferHelperAttrib vertUV = new VertexBufferHelperAttrib();
+                vertUV.Name = "_u0";
+                vertUV.Data = meshes[0].uvs.ToArray();
+                vertUV.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_16_16_Single;
+                VertexBufferHelperAttrib vertNorm = new VertexBufferHelperAttrib();
+                vertNorm.Name = "_n0";
+                vertNorm.Data = meshes[0].norms.ToArray();
+                vertNorm.Format = Syroot.NintenTools.Bfres.GX2.GX2AttribFormat.Format_10_10_10_2_SNorm;
+                atrib.Add(vertPos);
+                atrib.Add(vertNorm);
+                atrib.Add(vertUV);
+                helper.Attributes = atrib;
+                input.VertexBuffers[0] = helper.ToVertexBuffer();
+
+            }
             public class SMesh
             {
                 public string Name;
@@ -83,10 +124,16 @@ namespace BFRES_Injector_CMD
                 public List<Syroot.Maths.Vector4F> norms = new List<Syroot.Maths.Vector4F>();
                 //Last of vec4 is 0 fornow
                 public List<Syroot.Maths.Vector4F> uvs = new List<Syroot.Maths.Vector4F>();
+                public List<Syroot.Maths.Vector4F> boneINDX = new List<Syroot.Maths.Vector4F>();
+                public List<Syroot.Maths.Vector4F> boneWGHT = new List<Syroot.Maths.Vector4F>();
                 public IList<uint> faces = new List<uint>();
                 public List<List<string>> rawFace = new List<List<string>>();
             }
             public List<SMesh> meshes = new List<SMesh>();
+            public void ReadSMD(string File,Skeleton BaseModel)
+            {
+
+            }
             public void ReadObj(string File)
             {
                 List<Syroot.Maths.Vector4F> verts = new List<Syroot.Maths.Vector4F>();
@@ -156,210 +203,6 @@ namespace BFRES_Injector_CMD
 
             }
 
-        }
-        public class FileOutput
-        {
-            public static int fromFloat(float fval, bool littleEndian)
-            {
-                int fbits = FileOutput.SingleToInt32Bits(fval, littleEndian);
-                int sign = fbits >> 16 & 0x8000;          // sign only
-                int val = (fbits & 0x7fffffff) + 0x1000; // rounded value
-
-                if (val >= 0x47800000)               // might be or become NaN/Inf
-                {                                     // avoid Inf due to rounding
-                    if ((fbits & 0x7fffffff) >= 0x47800000)
-                    {                                 // is or must become NaN/Inf
-                        if (val < 0x7f800000)        // was value but too large
-                            return sign | 0x7c00;     // make it +/-Inf
-                        return sign | 0x7c00 |        // remains +/-Inf or NaN
-                            (fbits & 0x007fffff) >> 13; // keep NaN (and Inf) bits
-                    }
-                    return sign | 0x7bff;             // unrounded not quite Inf
-                }
-                if (val >= 0x38800000)               // remains normalized value
-                    return sign | val - 0x38000000 >> 13; // exp - 127 + 15
-                if (val < 0x33000000)                // too small for subnormal
-                    return sign;                      // becomes +/-0
-                val = (fbits & 0x7fffffff) >> 23;  // tmp exp for subnormal calc
-                return sign | ((fbits & 0x7fffff | 0x800000) // add subnormal bit
-                    + (0x800000 >> val - 102)     // round depending on cut off
-                    >> 126 - val);   // div by 2^(1-(exp-127+15)) and >> 13 | exp=0
-            }
-            List<byte> data = new List<byte>();
-
-            public byte[] getBytes()
-            {
-                return data.ToArray();
-            }
-
-            public void writeString(String s)
-            {
-                char[] c = s.ToCharArray();
-                for (int i = 0; i < c.Length; i++)
-                    data.Add((byte)c[i]);
-            }
-
-            public int size()
-            {
-                return data.Count;
-            }
-
-            public void writeOutput(FileOutput d)
-            {
-                foreach (byte b in d.data)
-                    data.Add(b);
-            }
-
-            private static char[] HexToCharArray(string hex)
-            {
-                return Enumerable.Range(0, hex.Length)
-                                 .Where(x => x % 2 == 0)
-                                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                                 .Select(x => Convert.ToChar(x))
-                                 .ToArray();
-            }
-
-            public void writeHex(string s)
-            {
-                char[] c = HexToCharArray(s);
-                for (int i = 0; i < c.Length; i++)
-                    data.Add((byte)c[i]);
-            }
-
-            public void writeInt(int i)
-            {
-
-                data.Add((byte)((i >> 24) & 0xFF));
-                data.Add((byte)((i >> 16) & 0xFF));
-                data.Add((byte)((i >> 8) & 0xFF));
-                data.Add((byte)((i) & 0xFF));
-
-            }
-
-            public void writeIntAt(int i, int p)
-            {
-                data[p++] = (byte)((i >> 24) & 0xFF);
-                data[p++] = (byte)((i >> 16) & 0xFF);
-                data[p++] = (byte)((i >> 8) & 0xFF);
-                data[p++] = (byte)((i) & 0xFF);
-            }
-            public void writeShortAt(int i, int p)
-            {
-                data[p++] = (byte)((i >> 8) & 0xFF);
-                data[p++] = (byte)((i) & 0xFF);
-            }
-
-            public void align(int i)
-            {
-                while (data.Count % i != 0)
-                    writeByte(0);
-            }
-
-            public void align(int i, int v)
-            {
-                while (data.Count % i != 0)
-                    writeByte(v);
-            }
-
-            /*public void align(int i, int value){
-                while(data.size() % i != 0)
-                    writeByte(value);
-            }*/
-
-
-            public void writeFloat(float f)
-            {
-                int i = SingleToInt32Bits(f, false);
-                data.Add((byte)((i) & 0xFF));
-                data.Add((byte)((i >> 8) & 0xFF));
-                data.Add((byte)((i >> 16) & 0xFF));
-                data.Add((byte)((i >> 24) & 0xFF));
-            }
-
-            public void writeFloatAt(float f, int p)
-            {
-                int i = SingleToInt32Bits(f, true);
-                data[p++] = (byte)((i) & 0xFF);
-                data[p++] = (byte)((i >> 8) & 0xFF);
-                data[p++] = (byte)((i >> 16) & 0xFF);
-                data[p++] = (byte)((i >> 24) & 0xFF);
-            }
-
-            public static int SingleToInt32Bits(float value, bool littleEndian)
-            {
-                byte[] b = BitConverter.GetBytes(value);
-                int p = 0;
-
-                if (!littleEndian)
-                {
-                    return (b[p++] & 0xFF) | ((b[p++] & 0xFF) << 8) | ((b[p++] & 0xFF) << 16) | ((b[p++] & 0xFF) << 24);
-                }
-                else
-                    return ((b[p++] & 0xFF) << 24) | ((b[p++] & 0xFF) << 16) | ((b[p++] & 0xFF) << 8) | (b[p++] & 0xFF);
-            }
-
-            public void writeHalfFloat(float f)
-            {
-                int i = fromFloat(f, false);
-                data.Add((byte)((i >> 8) & 0xFF));
-                data.Add((byte)((i) & 0xFF));
-            }
-
-            public void writeShort(int i)
-            {
-                data.Add((byte)((i >> 8) & 0xFF));
-                data.Add((byte)((i) & 0xFF));
-
-            }
-
-            public void writeByte(int i)
-            {
-                data.Add((byte)((i) & 0xFF));
-            }
-            public void writeSByte(int i)
-            {
-
-                 
-                data.Add((byte)((i) >> 0x1F + ((i) & 0x7F)));
-            }
-
-            public void writeChars(char[] c)
-            {
-                foreach (char ch in c)
-                    writeByte(Convert.ToByte(ch));
-            }
-
-            public void writeBytes(byte[] bytes)
-            {
-                foreach (byte b in bytes)
-                    writeByte(b);
-            }
-
-            public void write10sNorm(Vector4 vec)
-            {
-                //Vector3 norm = new Vector3(~(((int)(vec.X*511)*-1)-1), ~(((int)(vec.Y*551) * -1) - 1),~(((int)(vec.Z*511) * -1) - 1));
-                Vector3 norm = new Vector3((int)(vec.X * 511), (int)(vec.Y * 511), (int)(vec.Z * 511));
-                int Normal = ((int)norm.X) + (((int)norm.Y) << 10) + (((int)norm.Z) << 20);
-                writeInt(Normal);
-            }
-
-            public void writeFlag(bool b)
-            {
-                if (b)
-                    writeByte(1);
-                else
-                    writeByte(0);
-            }
-
-            public int pos()
-            {
-                return data.Count;
-            }
-
-            public byte[] save()
-            {
-                return data.ToArray();
-            }
         }
     }
 }
